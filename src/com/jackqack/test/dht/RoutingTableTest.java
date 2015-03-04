e4de33fb422388c93788b82a88dcee70a0785860
Tkachenko Alexander <skyalexx@gmail.com>
2015-03-04 18:49:22 +0300

    Added routing table tests(not tests actually)

--- src/com/jackqack/test/dht/RoutingTableTest.java
+++ src/com/jackqack/test/dht/RoutingTableTest.java
@@ -3,36 +3,65 @@ package com.jackqack.test.dht;
 import com.jackqack.dht.Key;
 import com.jackqack.dht.Node;
 import com.jackqack.dht.RoutingTable;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.logging.Logger;
 
 /**
  * Created by jackqack on 3/4/15.
  */
-public class RoutingTableTest {
 
-    @Test
-    public void seenNodeTest() {
-        Node[] nodes = new Node[7];
+/*
+ * Tests not created because it depends from constants, that
+ * may be changed. That is checked manually and works good at this stage.
+ */
+public class RoutingTableTest {
+    private static final Logger LOG = Logger.getLogger(RoutingTable.class.toString());
+
+    RoutingTable table;
+    Node[] nodes;
+
+
+    @Before
+    public void beforeTest(){
+        nodes = new Node[7];
         nodes[0] = new Node(new Key(Integer.parseInt("0000", 2)), "A");
         nodes[1] = new Node(new Key(Integer.parseInt("0001", 2)), "B");
-        nodes[2] = new Node(new Key(Integer.parseInt("0011", 2)), "C");
+        nodes[2] = new Node(new Key(Integer.parseInt("0001", 2)), "B");
         nodes[3] = new Node(new Key(Integer.parseInt("0100", 2)), "D");
         nodes[4] = new Node(new Key(Integer.parseInt("1001", 2)), "E");
         nodes[5] = new Node(new Key(Integer.parseInt("1010", 2)), "F");
         nodes[6] = new Node(new Key(Integer.parseInt("1111", 2)), "G");
 
-        RoutingTable table = new RoutingTable(nodes[6]);
+        table = new RoutingTable(nodes[5]);
 
         for(Node n: nodes) {
             table.seenNode(n);
         }
 
-        System.out.println(table.toString());
-
-        System.out.println("RoutingTableTest.seenNodeTest was successfull!");
-
+        LOG.info(table.toString());
     }
 
+    @Test
+    public void getClosestNodesTest(){
+        StringBuilder sb = new StringBuilder();
+        Node find = new Node(new Key(Integer.parseInt("0011", 2)), "C");
+        Node[] closestNodes = table.getClosestNodes(find.getKey());
+        Node[] neighbors = table.getNeighbors();
+
+        sb.append("\nClosestNodes to " + find.toString()  + " length: " + String.valueOf(closestNodes.length) + "\n");
+        for(Node n: closestNodes)
+            sb.append(n.toString() + "; ");
+
+        sb.append("\nNeighbors of " + table.myNode.toString() + " length: " + String.valueOf(neighbors.length) + "\n");
+        for(Node n: neighbors)
+            sb.append(n.toString() + "; ");
+
+
+        sb.append("\nTest was successfull!");
+        LOG.info(sb.toString());
+    }
 
 
 }
